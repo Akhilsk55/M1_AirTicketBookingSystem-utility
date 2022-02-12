75042a2ee2385b231c46821c01443ed1276de30c
Akhil <akhilsk555@gmail.com>
2022-02-12 19:12:43 +0530

    final commit

--- 3_Implementation/src/reservation.c
+++ 3_Implementation/src/reservation.c
@@ -8,17 +8,15 @@ void reserve()
         printf("\n");       
         printf("\tPLEASE SELECT HOW MANY SEATS : ");
         scanf("%d",&select_person[i].seats);
-        fflush(stdin); 
         printf("\n");
         printf("\tPLEASE ENTER YOUR NAME : ");
         scanf("%s",select_person[i].name);
-        fflush(stdin);printf("\n");
+        printf("\n");
         printf("\tFROM  : ");
         scanf("%s",select_person[i].from);
-        fflush(stdin);printf("\n");
+        printf("\n");
         printf("\tTO  : ");
         scanf("%s",select_person[i].to);
-        fflush(stdin);
         system("clear");
         booking++;
     
